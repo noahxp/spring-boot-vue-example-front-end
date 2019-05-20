699f649fa2b192b2aa31eedbf52ba63744b04366
Noah Liu <noahxp@gmail.com>
2019-05-20 15:53:01 +0800

    avoid cache in static file url

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-// vue config ,  configuration reference : https://cli.vuejs.org/config/#vue-config-js
+// vue config ,  configuration reference : https://cli.vuejs.org/config/#vue-config-js  , https://github.com/vuejs/vue-cli/issues/1967
+
+const staticFileVersion = new Date().getTime();
 
 module.exports = {
   filenameHashing: false,
@@ -6,5 +8,30 @@ module.exports = {
   devServer: {
     port: 8888,
     proxy: 'http://localhost:8080'
+  },
+
+  /*
+   * add timestamp to url, avoid cache
+   */
+  chainWebpack: (config) => {
+    config.module
+    .rule('images')
+    .use('url-loader')
+    .tap(options => Object.assign({}, options, { name: '[name].[ext]?ver=' + staticFileVersion }));
+  },
+  css: {
+    extract: {
+      filename: '[name].css?ver=' + staticFileVersion,
+      chunkFilename: '[name].css?ver=' + staticFileVersion,
+    },
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].js?ver=' + staticFileVersion,
+      chunkFilename: '[name].js?ver=' + staticFileVersion,
+    }
   }
+
 }
+
+
